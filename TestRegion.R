50c6027766e357ebcecf2d4e2d87bb281a5c6599
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2022-04-20 16:57:53 -0700

    identify interesting subset

--- TestRegion.R
+++ TestRegion.R
@@ -1,12 +1,35 @@
-TestRegion <- data.table::fread("TestRegion.csv", nrows=3000)
+library(data.table)
 library(ggplot2)
-TestRegion[, distance := 1:.N]
-TestRegion.tall <- data.table::melt(TestRegion,id="distance")
-TestRegion.tall[, depth := as.integer(sub("V","", variable))]
+skip <- 1400
+nrows <- 200
+TestRegion <- data.table::fread("TestRegion.csv", skip=skip, nrows=nrows)
+test.mat <- as.matrix(TestRegion)
+str(test.mat)
+image(test.mat)
 
-ggplot()+
+TestRegion.tall <- data.table(
+  intensity=as.numeric(test.mat),
+  depth.index=as.integer(col(test.mat)),
+  distance.shots=as.integer(row(test.mat)+skip))
+dist.hilite <- data.table(distance.shots=seq(1440, 1500, by=10))
+gg.img <- ggplot()+
   scale_fill_gradient(low="white", high="red")+
-  coord_equal()+
   geom_tile(aes(
-    distance, depth, fill=value),
-    data=TestRegion.tall)
+    distance.shots, depth.index, fill=intensity),
+    data=TestRegion.tall)+
+  scale_y_reverse()+
+  geom_vline(aes(
+    xintercept=distance.shots),
+    data=dist.hilite)
+png("TestRegion-zoom.png", width=20, height=10, units="in", res=50)
+print(gg.img)
+dev.off()
+
+intensity.hilite <- TestRegion.tall[dist.hilite, on="distance.shots"]
+ggplot()+
+  geom_point(aes(
+    depth.index, intensity),
+    data=intensity.hilite)+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0,"lines"))+
+  facet_grid(distance.shots ~ ., labeller=label_both)
